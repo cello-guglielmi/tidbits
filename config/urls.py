c37ccf7bd74d74570d96893478f4f2939a730eff
cello <marcello.guglielmi@outlook.com>
2025-06-26 00:39:36 -0300

    Project's urls.py import path fix.

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
-from quotes.views import HelpSection
+from quotes.views.public import HelpSection
 # from debug_toolbar import debug_toolbar_urls
 
 urlpatterns = [
